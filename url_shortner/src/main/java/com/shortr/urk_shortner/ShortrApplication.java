da9cd8425b4d65891bfa954a1430155c05044508
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-06-27 03:46:36 +0530

    Initial commit

---
+++ url_shortner/src/main/java/com/shortr/urk_shortner/ShortrApplication.java
@@ -0,0 +1,13 @@
+package com.shortr.urk_shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortrApplication.class, args);
+	}
+
+}
---
+++ url_shortner/src/test/java/com/shortr/urk_shortner/ShortrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortr.urk_shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
