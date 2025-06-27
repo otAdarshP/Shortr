5bb7c6d1988edd38895e00c07fc698d1f5112320
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-06-27 17:00:35 +0530

    models

--- url_shortner/src/main/java/models/User.java
+++ url_shortner/src/main/java/models/User.java
@@ -1,4 +1,19 @@
 package models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // automatically updates the Id of the users.
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private String role = "ROLE_USER";
+
 }
--- url_shortner/src/main/java/models/urlMapping.java
+++ url_shortner/src/main/java/models/urlMapping.java
@@ -1,4 +1,27 @@
 package models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
 public class urlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrl;
+    private String shortrUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne // many shortr urls for 1 user
+    @JoinColumn (name = "user_id")
+    private User user;
+
+
+    @OneToMany (mappedBy = "urlMapping") // many urls shortened at 1 time only
+    private List<clickEvent> clickEvents;
 }
