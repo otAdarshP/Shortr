4f5f73adbe9b5d3a09942734e6c562f2ceadb69d
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-06-27 17:12:44 +0530

    updates

--- url_shortner/src/main/java/models/clickEvent.java
+++ url_shortner/src/main/java/models/clickEvent.java
@@ -1,4 +1,20 @@
 package models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class clickEvent {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+
+
+    @ManyToOne
+    @JoinColumn (name = "url_mapping")
+    private urlMapping UrlMapping;
 }
